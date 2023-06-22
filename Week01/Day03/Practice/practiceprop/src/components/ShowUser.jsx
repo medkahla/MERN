9547ec099b146d1a33a28eba0b676ab4fefc0a39
medkahla <elkahlamed@gmail.com>
2023-06-22 18:57:06 +0100

    update for the assignment big inversion

--- Week01/Day03/Practice/practiceprop/src/components/ShowUser.jsx
+++ Week01/Day03/Practice/practiceprop/src/components/ShowUser.jsx
@@ -1,36 +1,46 @@
-import React, { Component } from 'react'
-import OneUser from './OneUser';
+// import React, { Component } from 'react'
+// import OneUser from './OneUser';
+// export default class ShowUser extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             person: this.props.person
+//         }
+//     }
+//     addAge(e,onePerson){
+//         console.log(onePerson);   
+//     }
+//     render() {
+//     return (
+//       <div>
+//             {this.state.person.map((person, i)=>{
+//                 return <OneUser key={i} person = {person}/>
+//             })}
+//       </div>
+//     )
+//   }
+// }
 
-export default class ShowUser extends Component {
-    
-    constructor(props){
-        super(props);
-        this.state = {
-            person: this.props.person
-        }
-    }
 
-    addAge(e,onePerson){
 
-        // console.log(onePerson);
-        // console.log(onePerson.age);
-        // onePerson.age = onePerson.age +1
+// Big inversion Assignment
+import React from 'react'
 
-        console.log(onePerson);
-        
-    }
-    
-    render() {
-
-    return (
-      <div>
-            {/* {JSON.stringify(person)}
-            {console.log(person[0])} */}
-            {this.state.person.map((person, i)=>{
-                return <OneUser key={i} person = {person}/>
+function ShowUser(props) {
+  const person = props.person
+  return (
+    <div>
+        {person.map((onePerson, i)=>{
+                return(
+                    <div key={i}>
+                        <h2>{onePerson.lastName}, {onePerson.firstName}</h2>
+                        Age: {onePerson.age} <br/>
+                        Hair color: {onePerson.hairColor} <br />
+                    </div>
+                ) 
             })}
-
-      </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default ShowUser
