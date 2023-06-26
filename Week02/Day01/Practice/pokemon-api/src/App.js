e06482f0709619734b99ecd7e87caadde4e0ac96
medkahla <elkahlamed@gmail.com>
2023-06-26 14:55:19 +0100

    Updated with Axios

--- Week02/Day01/Practice/pokemon-api/src/App.js
+++ Week02/Day01/Practice/pokemon-api/src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
   const [pokemons, setPokemons] = useState([])
   const fetchPokemons = ()=>{
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
-      .then(response =>{
-        return response.json();
-      }).then((requestedPokemons)=>{
-        setPokemons(requestedPokemons.results)
-      }).catch(err => {
-          console.log(err);
-      })
+    // fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+    //   .then(response =>{
+    //     return response.json();
+    //   }).then((requestedPokemons)=>{
+    //     setPokemons(requestedPokemons.results)
+    //   }).catch(err => {
+    //       console.log(err);
+    //   })
+
+    //  WITH AXIOS
+    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=807').then(response=>{
+      setPokemons(response.data.results)
+    })
   }
 
   return (
